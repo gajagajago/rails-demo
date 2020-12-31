0897bb7a00e2bb65eb73a01a4309e2e06cfa84fe
gajagajago <gajagajago@naver.com>
2020-12-31 16:52:44 +0900

    category views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,34 @@
 class CategoriesController < ApplicationController
-  def index
+  before_action :set_category, only: [:show]
 
+  def index
+    @category = Category.order('id DESC').paginate(page: params[:page], per_page: 15)
   end
 
   def show
-
   end
 
   def new
-
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
 
+    if(@category.save)
+      flash[:notice] = "카테고리: #{@category.name}가 생성되었습니다!"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+
+  before_save :downcase_fields
+
+  def downcase_fields
+    self.name.downcase!
+  end
 end
