689c1a7735b3ea47521c2c77f73faee9dee113a4
gajagajago <gajagajago@naver.com>
2021-01-11 01:05:17 +0900

    오타

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://always-rahv.herokuapp.com//cable'
+  config.action_cable.url = 'wss://always-rahv.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://always-rahv.herokuapp.com', /http:\/\/always-rahv.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
